74ef4640fb3e40a5a46c56c13d8582f20e185a46
enricomazzotti <enrico.mazzotti3@gmail.com>
2024-04-09 18:48:44 +0200

    refactor

--- Pomodoro/pomodoro.js
+++ Pomodoro/pomodoro.js
@@ -7,11 +7,6 @@ startStudySession = function (studyTime, restTime) {
   element.setAttribute("hidden", "hidden");
        
 
-  /*
-  document.querySelector(".studyBlob").style.animation = 'none';
-  document.querySelector(".restBlob").style.animation = 'none';
-  */
-
   const styleSheet = document.createElement("style");
   styleSheet.innerText = `
     .studyBlob{
@@ -45,20 +40,14 @@ startStudySession = function (studyTime, restTime) {
   document.head.appendChild(styleSheet);
 }
 
-// Aggiunge un listener per l'evento di submit al form con id 'studyForm'
+
 document.getElementById('studyForm').addEventListener('submit', function (event) {
   event.preventDefault();
 
-  // Ottiene il tempo di studio inserito dall'utente e lo converte in un numero intero
+
   let studyTime = parseInt(document.getElementById('studyTime').value, 10);
   let restTime = parseInt(document.getElementById('restTime').value, 10);
   let studyCycle = parseInt(document.getElementById('studyCycle').value, 10);
-  /*
-  console.log(studyTime);
-  console.log(restTime);
-  console.log(studyCycle);
-  */
-  //startStudySession(studyTime, restTime);
 
   for (let i = 0; i < studyCycle; i++) {
     setTimeout(function () {
@@ -100,37 +89,5 @@ document.getElementById('studyForm').addEventListener('submit', function (event)
       document.getElementById('timerDisplay').textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
     }
   }, 1000);
-
-  /*
-
-  
-
-  // Calcola il tempo di fine aggiungendo la durata del timer al tempo corrente
-  
-
-  // Imposta un intervallo che si ripete ogni secondo
-  const interval = setInterval(function () {
-    const now = Date.now();
-    // Calcola la differenza tra il tempo di fine e il tempo corrente
-    const difference = endTime - now;
-
-    // Se la differenza è minore o uguale a 0, ferma l'intervallo
-    if (difference <= 0) {
-      clearInterval(interval);
-      // Pulisce il testo dell'elemento con id 'timerDisplay'
-      document.getElementById('timerDisplay').textContent = "";
-
-      return;
-    }
-
-    // Calcola i minuti e i secondi rimanenti
-    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-
-    // Visualizza il tempo rimanente nell'elemento con id 'timerDisplay'
-    //padstart aggiunge uno zero prima della stringa se non raggiunge almeno una lunghezza di 2
-    document.getElementById('timerDisplay').textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-  }, 1000);
-  */
 });
 
